8c4bb6fd9776416fb50d56acffb6a2ee9c6d5bb3
Daniel Jahn <danieljahn1992@gmail.com>
2021-02-23 22:59:28 +0100

    Changed to switch case. Added cereal command with no functionality so far.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Discord = require("discord.js");
+const { write } = require("fs");
 const config = require("./config.json");
 
 const client = new Discord.Client();
 
-const prefix = "!";
+const prefix = "/";
+
+// client.on("typingStart", typingStart => {
+//    console.log("typing");
+//    console.dir(typingStart);
+// });
 
 client.on("message", function (message) {
   if (message.author.bot) return;
@@ -13,13 +19,20 @@ client.on("message", function (message) {
   const args = commandBody.split(" ");
   const command = args.shift().toLowerCase();
 
-  if (command === "ping") {
-    const timeTaken = Date.now() - message.createdTimestamp;
-    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-  } else if (command === "sum") {
-    const numArgs = args.map((x) => parseFloat(x));
-    const sum = numArgs.reduce((counter, x) => (counter += x));
-    message.reply(`The sum of all the arguments you provided is ${sum}!`);
+  switch (command) {
+    case "ping":
+      const timeTaken = Date.now() - message.createdTimestamp;
+      message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+      break;
+    case "sum":
+      const numArgs = args.map((x) => parseFloat(x));
+      const sum = numArgs.reduce((counter, x) => (counter += x));
+      message.reply(`The sum of all the arguments you provided is ${sum}!`);
+      break;
+    case "cereal":
+      message.reply("Some Cereal stuff happening soon! (╯°□°）╯︵ ┻━┻");
+      break;
+    default:
   }
 });
 
