a7031fc36d4c99a098fb2498f9ecce9ae33876b5
Jonas Gerth <j.b.gerth@pm.me>
2021-02-28 23:09:58 +0100

    Fix CI

--- tests/messages.utils.test.js
+++ tests/messages.utils.test.js
@@ -1,7 +1,4 @@
-import MockDiscord from "./mock.discord";
 import utils from "../lib/messages/utils";
-
-const discord = new MockDiscord()
 
 test("adds 1 + 2 to equal 3", () => {
   expect(utils.sum([1, 2])).toBe(3);
