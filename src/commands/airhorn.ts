3b6bdd2bfb9f25757fde8aac574f37938cd1fecb
Jonas <60644944+jbgerth@users.noreply.github.com>
2021-03-13 21:25:10 +0100

    #4: Add Airhorn back
    * Add js opus and remove lib
    
    * Add sounds
    
    * Clean rebase
    
    * Make pretty
    
    * Change let to const
    
    * Fix CI
    
    * Fix tests

---
+++ src/commands/airhorn.ts
@@ -0,0 +1,39 @@
+import fs from "fs";
+import { Message } from "discord.js";
+
+async function airhorn(message: Message) {
+  if (message.member.voice.channel) {
+    const connection = await message.member.voice.channel.join();
+
+    // Create a dispatcher
+    const dispatcher = connection.play(
+      fs.createReadStream("./resources/sounds/airhorn_default.wav"),
+      { type: "unknown" }
+    );
+
+    dispatcher.setVolume(1);
+
+    dispatcher.on("start", () => {
+      console.log("airhorn_default.wav is now playing!");
+    });
+
+    dispatcher.on("finish", () => {
+      console.log("airhorn_default.wav has finished playing!");
+      dispatcher.destroy();
+      connection.disconnect();
+    });
+
+    // Always remember to handle errors appropriately!
+    dispatcher.on("error", console.error);
+  }
+}
+
+module.exports = {
+  name: "airhorn",
+  description: "Make some noise!",
+  args: false,
+  usage: "",
+  execute(message: Message) {
+    airhorn(message);
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,54 @@
 import { Client, Collection, Message } from "discord.js";
 import config from "../config.json";
-import fs from 'fs';
+import fs from "fs";
 
 const client: Client = new Client();
 const prefix: string = config.PREFIX;
-let commands: Collection<string, CommandInterface> = new Collection()
-let commandFiles: Array<string> = fs.readdirSync('lib/commands').filter(file => file.endsWith('.js'))
+const commands: Collection<string, CommandInterface> = new Collection();
+const commandFiles: Array<string> = fs
+  .readdirSync("lib/commands")
+  .filter((file) => file.endsWith(".js"));
 
 client.login(config.BOT_TOKEN);
 
 export interface CommandInterface {
-    name: string;
-    desciption: string;
-    args: boolean;
-    usage: string;
-    execute(message: Message, args?: Array<string>);
+  name: string;
+  description: string;
+  args: boolean;
+  usage: string;
+  execute(message: Message, args?: Array<string>);
 }
 
-for(const file of commandFiles) {
-	const command = require(`./commands/${file}`)
-	commands.set(command.name, command)
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.set(command.name, command);
 }
 
 client.once("ready", () => {
-	console.log("Time to get cereal!");
+  console.log("Time to get cereal!");
 });
 
 client.on("message", (message: any) => {
-  	if (!message.content.startsWith(prefix) || message.author.bot) {
-		return;
-	}
+  if (!message.content.startsWith(prefix) || message.author.bot) {
+    return;
+  }
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLocaleLowerCase();
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLocaleLowerCase();
 
-	try {
-		const executable: CommandInterface = commands.get(command)
-		if(executable.args && !args.length) {
-			let reply = `You didn't provide any arguments, ${message.author}!`
-			if(executable.usage) {
-				reply += `\nThe proper usage would be: \`${prefix}${executable.name} ${executable.usage}\``
-			}
-			message.channel.send(reply)
-		} else {
-			commands.get(command).execute(message, args)
-		}
-	}
-	catch(error) {
-		console.error(error)
-		message.reply('I\'m sorry, but I couldn\'t execute that command.')
-	}
+  try {
+    const executable: CommandInterface = commands.get(command);
+    if (executable.args && !args.length) {
+      const reply = `You didn't provide any arguments, ${message.author}!`;
+      if (executable.usage) {
+        reply += `\nThe proper usage would be: \`${prefix}${executable.name} ${executable.usage}\``;
+      }
+      message.channel.send(reply);
+    } else {
+      commands.get(command).execute(message, args);
+    }
+  } catch (error) {
+    console.error(error);
+    message.reply("I'm sorry, but I couldn't execute that command.");
+  }
 });
