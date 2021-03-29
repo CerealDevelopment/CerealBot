29251f75362cb78bda277798ced7918859a72864
Jonas Gerth <j.b.gerth@pm.me>
2021-03-29 17:32:00 +0200

    Fix test

--- tests/commands.sum.test.js
+++ tests/commands.sum.test.js
@@ -1,5 +1,6 @@
 import sum from "../lib/commands/sum";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum.testable_fun([1, 2])).toBe(3);
+test("adds 1 + 2 to equal 3", async () => {
+  const result = await sum.testable_fun([1, 2]); 
+  expect(result).toBe(3);
 });
