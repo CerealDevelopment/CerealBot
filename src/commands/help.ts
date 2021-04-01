4a4b28f55f7b49f2de544ef4816daeba34f7b7aa
Daniel Jahn <danieljahn1992@gmail.com>
2021-04-01 18:59:40 +0200

    Finshed implementation of help command.

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,20 +1,44 @@
 import { Message } from "discord.js";
 import { getCommandMap } from "../utils";
+import config from "../../config.json";
 
-// const command_list = utils.getCommandMap;
+const prefix: string = config.PREFIX ? config.PREFIX : "!";
 
 function printHelp(message: Message, args: string[]) {
-  const commands = getCommandMap().keyArray();
   if (!args.length) {
     // Simple list of all commands if no arg is provided
+    const commandString = getCommandMap()
+      .keyArray()
+      .reduce(
+        (total: String, currentVal: String, index: number, array: string[]) => {
+          const joiner = "\n- ";
+          const str = `${total}${joiner}${currentVal}`;
+          if (index === 1) {
+            return `\`\`\`${joiner}${str}`;
+          } else if (index === array.length - 1) {
+            return `${str}\`\`\``;
+          } else {
+            return `${str}`;
+          }
+        }
+      );
+
     message.reply(
-      // TODO individual prefix
-      `Here is a list of all featured commands: 
-      ${commands}
-      For more specific help type !help <command>.`
+      `Here is a list of all featured commands:\n${commandString}\nFor more specific help type \`${prefix}help 'command_name'\`.`
     );
   } else {
     // More specific command help
+    const command = getCommandMap().get(args[0]);
+    if (!command) {
+      message.reply("Unknown command: " + args[0]);
+    } else {
+      let answer =
+        "\nName: " + command.name + "\nDescription: " + command.description;
+      if (command.usage) {
+        answer += "\nUsage: " + command.usage;
+      }
+      message.reply(answer);
+    }
   }
 }
 
