8852959a00d2db363075532940b1a0892705b899
Jonas Gerth <j.b.gerth@pm.me>
2021-04-08 19:32:16 +0200

    Add urban dict command Fixes #24

--- tests/setup.js
+++ tests/setup.js
@@ -1,11 +1,14 @@
-import fs from "fs"
+import fs from "fs";
 
 module.exports = async () => {
-    // Create config file for CI tests
-    global.CONFIG_FILE = fs.copyFile("config.template.json", "config.json", (err) => {
-        if (err) {
-          console.log("Config already exists");
-        }
-      });
-  };
-  
+  // Create config file for CI tests
+  global.CONFIG_FILE = fs.copyFile(
+    "config.template.json",
+    "config.json",
+    (err) => {
+      if (err) {
+        console.log("Config already exists");
+      }
+    }
+  );
+};
