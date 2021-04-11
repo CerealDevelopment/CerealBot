a07306ddfe413c51c31dcdb3db2b9124e65494a9
Jonas Gerth <j.b.gerth@pm.me>
2021-04-11 13:06:04 +0200

    Fix tests to not overwrite config

--- tests/setup.js
+++ tests/setup.js
@@ -1,14 +1,18 @@
 import fs from "fs";
 
-module.exports = async () => {
-  // Create config file for CI tests
-  global.CONFIG_FILE = fs.copyFile(
-    "config.template.json",
-    "config.json",
-    (err) => {
+const PATH_TO_CONFIG = "config.json";
+const createConfigIfNotExist = async (pathToConfig) => {
+  const configExists = fs.existsSync(PATH_TO_CONFIG);
+  if (!configExists) {
+    fs.copyFile("config.template.json", pathToConfig, (err) => {
       if (err) {
         console.log("Config already exists");
       }
-    }
-  );
+    });
+  }
+};
+
+module.exports = async () => {
+  // Create config file for CI tests
+  global.CONFIG_FILE = createConfigIfNotExist(PATH_TO_CONFIG);
 };
