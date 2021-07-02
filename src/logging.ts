911d4eea77eba2c8c0803edcd11a38689416ae82
Jonas Gerth <j.b.gerth@pm.me>
2021-07-02 23:42:31 +0200

    Remove redundat comments

--- src/logging.ts
+++ src/logging.ts
@@ -11,19 +11,11 @@ const logger = createLogger({
     format.json()
   ),
   transports: [
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //
-    new transports.File({ filename: "error.log", level: "error" }),
-    new transports.File({ filename: "combined.log" }),
+    new transports.File({ filename: "./logs/error.log", level: "error" }),
+    new transports.File({ filename: "./logs/combined.log" }),
   ],
 });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
 if (process.env.NODE_ENV !== "production") {
   logger.add(
     new transports.Console({
