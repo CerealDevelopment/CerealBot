9209fe310371cf18548211d658902b942622e059
Andre Rother <andre@andrerother.de>
2021-07-10 17:22:56 +0200

    fix: Correct imports

--- src/commands/guild/prefix.ts
+++ src/commands/guild/prefix.ts
@@ -1,8 +1,7 @@
 import { Message } from "discord.js";
 import Keyv from "keyv";
-import { createError } from "../../utils";
 import { userPermissions } from "../../discordUserPermissions";
-import { DB } from "../../../config.json";
+import { DATABASE } from "../../../config.json";
 
 const getGuildPrefix = async (message: Message, guildPrefixes: Keyv): Promise<string> => {
   const guildId: string = message.guild.id;
@@ -16,24 +15,23 @@ const setGuildPrefix = async (message: Message, guildPrefixes: Keyv, newPrefix: 
   return `You changed the prefix to \`${newPrefix}\``;
 };
 
-const dispatchPrefixFun = async (message: Message, keyvGuildConfig: Keyv, args: string[]): Promise<[string, Error]> => {
-  let error: Error = null;
+const dispatchPrefixFun = async (message: Message, keyvGuildConfig: Keyv, args: string[]): Promise<string> => {
   let reply: string;
 
   if (!args.length) {
     reply = await getGuildPrefix(message, keyvGuildConfig);
   } else if (args.length > 1) {
     reply = "You provided too many arguments. The prefix should be exactly one with a length of one. E.g.: `$`";
-    error = createError(message, reply);
+    throw new Error(reply)
   } else if (args.length === 1) {
     if (args[0].length > 1) {
       reply = "The provided prefix is too long. Make sure it has a length of 1";
-      error = createError(message, reply);
+      throw new Error(reply)
     } else {
       reply = await setGuildPrefix(message, keyvGuildConfig, args[0]);
     }
   }
-  return [reply, error];
+  return reply;
 };
 
 module.exports = {
@@ -43,12 +41,11 @@ module.exports = {
   neededUserPermissions: [userPermissions.administrator, userPermissions.manage_guild],
   usage: "<new Prefix> - has to be a length of 1. Keep in mind to choose a symbol makes using the bot easier.",
   async execute(message: Message, args: string[]) {
-    const keyvGuildConfig: Keyv = new Keyv(DB.DEVELOPMENT.CONNECTION_STRING, {
+    const keyvGuildConfig: Keyv = new Keyv(DATABASE.CONNECTION_STRING, {
       namespace: "guildConfig",
     });
-    const [reply, error] = await dispatchPrefixFun(message, keyvGuildConfig, args);
+    const reply = await dispatchPrefixFun(message, keyvGuildConfig, args);
 
     message.reply(reply);
-    return error;
   },
 };
