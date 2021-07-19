b930366fd3a7f4ad03840755179ecb9125bbac0f
Jonas Gerth <j.b.gerth@pm.me>
2021-07-19 19:49:35 +0200

    Add first property based tests Fixes #57

--- tests/commands.misc.sum.test.js
+++ tests/commands.misc.sum.test.js
@@ -1,6 +1,21 @@
+import * as fc from "fast-check";
 import { sum } from "../lib/commands/misc/sum";
 
 test("adds 1 + 2 to equal 3", async () => {
   const result = await sum([1, 2]);
   expect(result).toBe(3);
+});
+
+
+test('should be bigger', async () => {
+  await fc.assert(
+    fc.asyncProperty(fc.array(fc.integer({min:0, max:Number.MAX_SAFE_INTEGER}), {minLength:2, maxLength:1000}), async (data) => {
+      const summed = await sum(data);
+      expect(summed).toEqual(expect.any(Number));
+
+      for (const item of data) {
+        expect(summed).toBeGreaterThanOrEqual(item);
+      }
+    })
+  );
 });
