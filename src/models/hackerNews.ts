719c44ee2cfb5791a47231719a46a60cc9a3f0ca
Jonas Gerth <j.b.gerth@pm.me>
2021-11-02 18:17:19 +0100

    Add HackerNews Model

---
+++ src/models/hackerNews.ts
@@ -0,0 +1,34 @@
+class HackerNews {
+  by: string;
+  descendants: number;
+  id: number;
+  kids: number[];
+  score: number;
+  time: number;
+  title: string;
+  type: string;
+  url: string;
+
+  constructor(
+    by: string,
+    descendants: number,
+    id: number,
+    kids: number[],
+    score: number,
+    time: number,
+    title: string,
+    type: string,
+    url: string
+  ) {
+    this.by = by;
+    this.descendants = descendants;
+    this.kids = kids;
+    this.score = score;
+    this.time = time;
+    this.title = title;
+    this.type = type;
+    this.url = url;
+  }
+}
+
+export { HackerNews };
